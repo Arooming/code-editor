52af38152c68cf15207545d1c9c1d401cf8b0551
seoAreum <1971236@hansung.ac.kr>
2024-07-05 17:55:20 +0900

    feat: 코드 에디터 구현

--- src/CodeEditor.tsx
+++ src/CodeEditor.tsx
@@ -1,7 +1,39 @@
+
+import "codemirror/lib/codemirror.css";
+import { Controlled as CodeMirror } from "react-codemirror2";
+
+import "codemirror/theme/monokai.css";
+import "codemirror/addon/hint/show-hint.css";
+import "codemirror/addon/fold/foldgutter.css";
+import { useState } from "react";
+
 const CodeEditor = () => {
+  const [code, setCode] = useState("// code");
+
   return (
-    <div>
-      <p>editor</p>
+    <div
+      style={{
+        display: "flex",
+        position: "relative",
+        width: "100%",
+        height: 100,
+      }}
+    >
+      <div style={{ width: 10, flex: "10px 0 0" }} />
+      <div style={{ flex: "1 1" }}>
+        <CodeMirror
+          value={code}
+          options={{
+            mode: "xml",
+            theme: "monokai",
+            lineNumbers: true,
+          }}
+          onBeforeChange={(editor, data, value) => {
+            console.log(code)
+            setCode(value);
+          }}
+        />
+      </div>
     </div>
   );
 };
