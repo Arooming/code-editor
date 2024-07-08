f6f13def343284b13d03f6b5b832d92d5d1c7ec1
seoAreum <1971236@hansung.ac.kr>
2024-07-09 01:21:39 +0900

    feat: 코드 에디터 구현

--- src/CodeEditor.tsx
+++ src/CodeEditor.tsx
@@ -1,40 +1,57 @@
-
+import "codemirror/addon/edit/closebrackets";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/matchbrackets";
+import "codemirror/addon/fold/brace-fold";
+import "codemirror/addon/fold/comment-fold";
+import "codemirror/addon/fold/foldcode";
+import "codemirror/addon/fold/foldgutter";
+import "codemirror/addon/fold/foldgutter.css";
+import "codemirror/addon/hint/javascript-hint";
+import "codemirror/addon/hint/show-hint";
+import "codemirror/addon/hint/show-hint.css";
+import "codemirror/keymap/sublime";
 import "codemirror/lib/codemirror.css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/theme/material.css";
+import "codemirror/theme/monokai.css";
+import { useState } from "react";
 import { Controlled as CodeMirror } from "react-codemirror2";
 
-import "codemirror/theme/monokai.css";
-import "codemirror/addon/hint/show-hint.css";
-import "codemirror/addon/fold/foldgutter.css";
-import { useState } from "react";
-
 const CodeEditor = () => {
-  const [code, setCode] = useState("// code");
+  const [code, setCode] = useState("");
+  const [mode, _] = useState("javascript");
+
+  const handleChangeCode = (newCode: string) => {
+    setCode(newCode);
+  };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        position: "relative",
-        width: "100%",
-        height: 100,
+    <CodeMirror
+      value={code}
+      options={{
+        mode: `${mode}`,
+        theme: "monokai",
+        lineWrapping: true,
+        smartIndent: true,
+        lineNumbers: true,
+        foldGutter: true,
+        gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+        autoCloseTags: true,
+        keyMap: "sublime",
+        matchBrackets: true,
+        autoCloseBrackets: true,
+        autofocus: true,
+        extraKeys: {
+          Tab: (cm) => cm.execCommand("indentAuto"),
+          "Cmd-/": "toggleComment",
+          "Ctrl-Space": "autocomplete",
+        },
       }}
-    >
-      <div style={{ width: 10, flex: "10px 0 0" }} />
-      <div style={{ flex: "1 1" }}>
-        <CodeMirror
-          value={code}
-          options={{
-            mode: "xml",
-            theme: "monokai",
-            lineNumbers: true,
-          }}
-          onBeforeChange={(editor, data, value) => {
-            console.log(code)
-            setCode(value);
-          }}
-        />
-      </div>
-    </div>
+      onBeforeChange={(_, __, value) => handleChangeCode(value)}
+      onChange={(_, __, value) => {
+        console.log(value);
+      }}
+    />
   );
 };
 
